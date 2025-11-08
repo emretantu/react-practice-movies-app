4fb928205aa907a83c9f6f5dcf087897b1172c37
emretantu <emretantu@gmail.com>
2025-11-08 14:51:23 +0300

    Implement header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Search from './components/search';
 
 function App() {
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <img src="./hero.png" alt="Hero Banner" />
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+    </main>
   )
 }
 
